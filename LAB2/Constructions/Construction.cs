0dc9e969e398b7e53c77bcc1f5ab951f6817491e
Yana Baharevich <w68369@student.wsiz.edu.pl>
2023-10-25 13:20:07 +0200

    init

---
+++ LAB2/Constructions/Construction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2.Constructions
+{
+    class Construction
+    {
+        public Construction(float height,float widht,int entrances,int humanCapacity,string buildMaterial)
+        {
+            Height = height;
+            Width = widht;
+            Entrances = entrances;
+            HumanCapacity = humanCapacity;
+            BuildMaterial = buildMaterial;
+            _BuildMaterial = buildMaterial;
+        }
+
+        private string _BuildMaterial { get; set; }
+        public float Height { get; set; }
+        public float Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+        public string BuildMaterial { 
+            get {
+                return _BuildMaterial;
+            } 
+            set { _BuildMaterial = value; 
+            } 
+        }
+    }
+}
---
+++ LAB2/Program.cs
@@ -0,0 +1,15 @@
+﻿using LAB2.Constructions;
+using System;
+
+namespace LAB2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var constr = new Construction(32,21,3,1,"aboba");
+            constr.BuildMaterial = "abobusy";
+            Console.WriteLine(constr.BuildMaterial);
+        }
+    }
+}
